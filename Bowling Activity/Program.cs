6ff8b92c54c7932dc44424b4b9ee5d8d400f4270
Emily Morgan <emorgan921@gmail.com>
2024-06-14 10:29:17 -0400

    Add project files.

---
+++ Bowling Activity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Versioning;
+
+namespace Bowling_Activity;
+
+internal class Program
+{
+   static void Main(string[] args)
+    {
+
+        List<int> scores = new List<int>();
+
+        for(int frame = 1;frame <= 10; frame++)
+             {
+             Console.Write($"Enter score for frame {frame}: ");
+             var response = Console.ReadLine();
+             var frameScore = Convert.ToInt32(response);
+             scores.Add(frameScore);
+             }
+   
+             var sum = 0;
+             foreach(var score in scores) {
+                sum += score;
+                }
+            Console.WriteLine($"Game score is {sum}");
+             
+   }
+
+}
